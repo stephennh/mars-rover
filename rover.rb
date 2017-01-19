1e3bd6459c598a8a47eaffb01e0a5d874e93e0f8
Stephen H <stephen210huang@hotmail.com>
2017-01-19 17:52:17 -0500

    rover.rb update with proper spacing

--- rover.rb
+++ rover.rb
@@ -19,7 +19,7 @@ class Rover
         else
           puts "Improper input"
         end
-        puts "The new rover position is #{ @x } and #{ @y}
+        puts "The new rover position is #{ @x } and #{ @y }
         with the direction of #{ @direction }"
       end
   end
