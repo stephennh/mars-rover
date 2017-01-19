56f6c169862fd40cf5fbd358bb1be59b925b7fda
Stephen H <stephen210huang@hotmail.com>
2017-01-19 17:39:44 -0500

    turn and move function programmed/non-tested

--- rover.rb
+++ rover.rb
@@ -13,21 +13,53 @@ class Rover
         if character == "M"
           move
         elsif character == "L"
-          turn
+          turn("L")
         elsif character == "R"
-          turn
+          turn("R")
         else
           puts "Improper input"
         end
       end
   end
 
-  def move(answer)
-
+  def move
+    case @direction
+    when "N"
+      @y += 1
+    when "E"
+      @x += 1
+    when "S"
+      @y -= 1
+    when "W"
+      @x -= 1
+    end
   end
 
   def turn(answer)
-
+    if answer == "L"
+      case @direction
+      when "N"
+        @direction = "W"
+      when "W"
+        @direction = "S"
+      when "S"
+        @direction = "E"
+      when "E"
+        @direction = "N"
+      end
+    end
+    if answer == "R"
+      case @direction
+      when "N"
+        @direction = "E"
+      when "E"
+        @direction = "S"
+      when "S"
+        @direction = "W"
+      when "W"
+        @direction = "N"
+      end
+    end
   end
 
 end
